048b6a25e76ac63a25b7e8cd432b32450467c6b9
ivan <vivanchi@gmail.com>
2025-12-13 05:10:45 -0300

    fixing 500 on register, userId issues

--- routes/auth.js
+++ routes/auth.js
@@ -19,8 +19,7 @@ router.post("/register", async (req, res) => {
 
     const user = await User.create({
       email,
-      password: hashed,
-      userId: user._id
+      password: hashed
     });
 
     return res.status(201).json({ message: "User created" });
